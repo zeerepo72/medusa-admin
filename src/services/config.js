bbbe65d607f18d597d0708cd000cdd19ea58a7e0
zeerepo72 <116797901+zeerepo72@users.noreply.github.com>
2022-11-07 17:21:14 +0500

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,6 +1,6 @@
 import { QueryClient } from "react-query"
 
-let medusaUrl = "http://localhost:9000"
+let medusaUrl = "https://clever-torte-c56cee.netlify.app"
 
 // deprecated
 if (process.env.GATSBY_STORE_URL) {
